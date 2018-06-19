18d700c65aa1dc32963bd01ca3ff655b8b361bc0
chencheng-chan <103996441@qq.com>
2018-06-20 01:02:26 +0800

    生成文件提交

---
+++ Solution.DataAccess/SubSonic/DUE01Table.cs
@@ -0,0 +1,133 @@
+ 
+using System;
+
+namespace Solution.DataAccess.DataModel {
+        /// <summary>
+        /// Table: DUE01
+        /// </summary>
+
+        public class DUE01Table {
+			/// <summary>
+			/// 表名
+			/// </summary>
+			public static string TableName {
+				get{
+        			return "DUE01";
+      			}
+			}
+
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string Id{
+			      get{
+        			return "Id";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string SHOP_ID{
+			      get{
+        			return "SHOP_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string TAKEIN_ID{
+			      get{
+        			return "TAKEIN_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string SNO{
+			      get{
+        			return "SNO";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string PROD_ID{
+			      get{
+        			return "PROD_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string STD_UNIT{
+			      get{
+        			return "STD_UNIT";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string STD_QUAN{
+			      get{
+        			return "STD_QUAN";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string STD_PRICE{
+			      get{
+        			return "STD_PRICE";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string Tax{
+			      get{
+        			return "Tax";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string QUAN1{
+			      get{
+        			return "QUAN1";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string QUAN2{
+			      get{
+        			return "QUAN2";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string ITEM_DISC_AMT{
+			      get{
+        			return "ITEM_DISC_AMT";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string MEMO{
+			      get{
+        			return "MEMO";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string BAT_NO{
+			      get{
+        			return "BAT_NO";
+      			}
+		    }
+                    
+        }
+}
---
+++ Solution.DataAccess/SubSonic/SHOP_ACCOUNTTable.cs
@@ -0,0 +1,85 @@
+ 
+using System;
+
+namespace Solution.DataAccess.DataModel {
+        /// <summary>
+        /// Table: SHOP_ACCOUNT
+        /// </summary>
+
+        public class SHOP_ACCOUNTTable {
+			/// <summary>
+			/// 表名
+			/// </summary>
+			public static string TableName {
+				get{
+        			return "SHOP_ACCOUNT";
+      			}
+			}
+
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string Id{
+			      get{
+        			return "Id";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string SHOP_ID{
+			      get{
+        			return "SHOP_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string HEAD_SHOP_ID{
+			      get{
+        			return "HEAD_SHOP_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string AMOUNT{
+			      get{
+        			return "AMOUNT";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string Memo{
+			      get{
+        			return "Memo";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string CRT_DATETIME{
+			      get{
+        			return "CRT_DATETIME";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string CRT_USER_ID{
+			      get{
+        			return "CRT_USER_ID";
+      			}
+		    }
+			/// <summary>
+			/// 
+			/// </summary>
+   			public static string MOD_DATETIME{
+			      get{
+        			return "MOD_DATETIME";
+      			}
+		    }
+                    
+        }
+}
