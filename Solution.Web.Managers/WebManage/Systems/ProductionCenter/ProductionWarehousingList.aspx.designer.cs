8d790a5e1f460a72f586f98cef06239cec650ecd
Hansen1989 <415078405@qq.com>
2018-12-20 09:01:43 +0800

    dd

--- Solution.Web.Managers/WebManage/Systems/ProductionCenter/ProductionWarehousingList.aspx.designer.cs
+++ Solution.Web.Managers/WebManage/Systems/ProductionCenter/ProductionWarehousingList.aspx.designer.cs
@@ -112,6 +112,15 @@ namespace Solution.Web.Managers.WebManage.Systems.ProductionCenter {
         protected global::FineUI.Button Button2;
         
         /// <summary>
+        /// ButtonImport 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Button ButtonImport;
+        
+        /// <summary>
         /// ButtonApproval 控件。
         /// </summary>
         /// <remarks>
