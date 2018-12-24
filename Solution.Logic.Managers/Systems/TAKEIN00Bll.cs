24aebb0cca777e76dee845dab060a69a5e25b86b
Hansen1989 <415078405@qq.com>
2018-12-24 23:21:05 +0800

    添加TakeinBll

---
+++ Solution.Logic.Managers/Systems/TAKEIN00Bll.cs
@@ -0,0 +1,30 @@
+﻿using Solution.DataAccess.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution.Logic.Managers
+{
+    public partial class TAKEIN00Bll:LogicBase
+    {
+        public int LeadIntoProductPlanList(string beginTime,string endTime,string isChoseTime,string shopId,string userId)
+        {
+            int error = 0;
+            try
+            {
+                SPs.LeadIntoProductPlanList(beginTime,endTime,isChoseTime,shopId,userId).Execute();
+
+            }
+            catch (Exception)
+            {
+                error = 1;
+            }
+
+            return error;
+
+        }
+
+
+    }
+}
